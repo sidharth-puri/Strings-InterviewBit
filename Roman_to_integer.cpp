35269e004109488dae46823c6a3658217c3d5af8
Sidharth Puri <52946626+sidharth-puri@users.noreply.github.com>
2020-03-06 21:54:13 +0530

    Create Roman_to_integer.cpp

---
+++ Roman_to_integer.cpp
@@ -0,0 +1,34 @@
+int value(char r)
+{
+    if(r=='I') return 1;
+    if(r=='V') return 5;
+    if(r=='X') return 10;
+    if(r=='L') return 50;
+    if(r=='C') return 100;
+    if(r=='D') return 500;
+    if(r=='M') return 1000;
+}
+int Solution::romanToInt(string str) {
+    int res=0,i;
+    for(i=0;i<str.length();i++)
+    {
+        int s1=value(str[i]);
+        if(i+1<str.length())
+        {
+            int s2=value(str[i+1]);
+            if(s1>=s2)
+            {
+                res=res + s1;
+            }
+            if(s1<s2)
+            {
+                res=res + s2 -s1;
+                i++;
+            }
+        }
+        else{
+            res=res + s1;
+        }
+    }
+    return res;
+}
