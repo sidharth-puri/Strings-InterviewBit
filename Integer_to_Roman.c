e444ffea5ce968eae88f005fb939eeb5e3cb8877
Sidharth Puri <52946626+sidharth-puri@users.noreply.github.com>
2020-06-03 18:43:54 +0530

    Update Integer_to_Roman.c

--- Integer_to_Roman.c
+++ Integer_to_Roman.c
@@ -1,8 +1,4 @@
-/**
- * @input A : Integer
- * 
- * @Output string. Make sure the string ends with null character
- */
+
 char* intToRoman(int A) {
     int num[] = {1,4,5,9,10,40,50,90,100,400,500,900,1000}; 
     char** sym = {"I","IV","V","IX","X","XL","L","XC","C","CD","D","CM","M"}; 
